d05559519b4e9cd17c45875c2e6792c5b0e33d34
Maxim Akkuzin <code@makkuzin.name>
2017-02-26 18:51:30 +0300

    Add bindings for mouse emulation

--- xmonad.hs
+++ xmonad.hs
@@ -59,6 +59,8 @@ main = do
     , normalBorderColor = myBorderColor
     , focusedBorderColor = myActiveBorderColor
     } `additionalKeys`
+    -- mod1Mask - altKey, mod4Mask - winKey
+    -- http://hackage.haskell.org/package/X11-1.8/docs/Graphics-X11-Types.html
     [ ((mod1Mask .|. controlMask, xK_l), spawn "xscreensaver-command -lock")
     , ((controlMask, xK_Print), spawn "scrot -u")
     , ((0, xK_Print), spawn "scrot")
@@ -72,4 +74,18 @@ main = do
     , ((mod1Mask, xK_F1), sendMessage ToggleStruts)
     , ((mod1Mask, xK_F2), spawn "dmenu_run")
     , ((mod1Mask, xK_F4), kill)
+    -- mouse
+    -- quick
+    , ((mod4Mask .|. shiftMask, xK_j), spawn "xdotool mousemove_relative 0 40") -- down
+    , ((mod4Mask .|. shiftMask, xK_k), spawn "xdotool mousemove_relative 0 -40") -- up
+    , ((mod4Mask .|. shiftMask, xK_h), spawn "xdotool mousemove_relative -- -40 0") -- left
+    , ((mod4Mask .|. shiftMask, xK_l), spawn "xdotool mousemove_relative 40 0") -- right
+    -- slow
+    , ((mod4Mask, xK_j), spawn "xdotool mousemove_relative 0 10") -- down
+    , ((mod4Mask, xK_k), spawn "xdotool mousemove_relative 0 -10") -- up
+    , ((mod4Mask, xK_h), spawn "xdotool mousemove_relative -- -10 0") -- left
+    , ((mod4Mask, xK_l), spawn "xdotool mousemove_relative 10 0") -- right
+    -- clicks
+    , ((mod4Mask, xK_Return), spawn "xdotool click 1") -- left click
+    , ((mod4Mask, xK_backslash), spawn "xdotool click 3") -- right click
     ]
